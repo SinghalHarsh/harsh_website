7bd14224160758998e5822120f835558ff1c8c3e
Harsh Singhal <harshsinghal726@gmail.com>
2026-01-17 17:38:50 +0530

    Fix Netlify function: include app files in bundle

--- netlify/functions/api.py
+++ netlify/functions/api.py
@@ -5,10 +5,6 @@ import awsgi
 # Add the root directory to sys.path to allow importing app
 # This assumes this file is in netlify/functions/
 current_dir = os.path.dirname(os.path.abspath(__file__))
-root_dir = os.path.dirname(os.path.dirname(os.path.dirname(current_dir))) 
-# Actually, let's differ calculating root dir.
-# file is at /netlify/functions/api.py.
-# root is ../../
 root_dir = os.path.abspath(os.path.join(current_dir, "../../"))
 sys.path.insert(0, root_dir)
 
